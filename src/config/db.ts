bc6991ecefa6091acc6db36bb1ed7558fa18fc11
santiago1708 <vela123tique@gmail.com>
2024-11-27 18:52:17 -0500

    se conecto a Mongo con ODM

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+import colors from 'colors'
+import { exit } from 'node:process';
+
+export const connectDB = async () => {
+    try {
+        const {connection} = await mongoose.connect(process.env.DATABASE_URL)
+        const url = `${connection.host}:${connection.port}`
+        console.log(colors.magenta.bold(`MongoDB connection: ${url}`));
+    } catch (error) {
+        /* console.log(error.message); */
+        console.log(colors.bgRed.bold(`Error al conectar a MONGODB`))
+        exit(1);
+    }
+}
