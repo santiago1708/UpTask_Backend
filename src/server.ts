19c11619dae5a54fc0f4e4dcc6f7bf3f8ecfa8a5
santiago1708 <vela123tique@gmail.com>
2024-12-04 14:19:45 -0500

    se agrego morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
 import { corsConfig } from "./config/cors";
@@ -11,6 +12,9 @@ connectDB()
 const app = express();
 app.use(cors(corsConfig))
 
+//Login 
+
+app.use(morgan('dev'))
 app.use(express.json())
 /* Routes */
 app.use('/api/projects', projectRoutes)
