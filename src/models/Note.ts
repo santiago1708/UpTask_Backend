eec5f31999f38327d45facc3a88312704b19e6d6
santiago1708 <vela123tique@gmail.com>
2024-12-22 17:35:57 -0500

    se creo el modelo de notes

---
+++ src/models/Note.ts
@@ -0,0 +1,27 @@
+import moongose, { Schema, Document, Types } from 'mongoose'
+
+export interface INote extends Document {
+    content: string
+    createby: Types.ObjectId
+    task: Types.ObjectId
+}
+
+const noteSchema: Schema = new Schema({
+    constent: {
+        type: String,
+        required: true
+    },
+    createby: {
+        type: Types.ObjectId,
+        ref: 'Auth',
+        required: true
+    },
+    task: {
+        type: Types.ObjectId,
+        ref: 'Task',
+        required: true
+    }
+}, {timestamps: true})
+
+const Note = moongose.model<INote>('Note', noteSchema)
+export default Note
